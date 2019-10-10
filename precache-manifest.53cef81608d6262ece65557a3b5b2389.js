8ea463cd61af5b552fc98b4da46616f1d6bae1af
Ayushi Jha <jhaayushijha@gmail.com>
2019-10-10 01:50:46 -0700

    Updates

---
+++ precache-manifest.53cef81608d6262ece65557a3b5b2389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26acca974d78c64b30cd1aa678f0fc9f",
+    "url": "/Rich-Text-Editor/index.html"
+  },
+  {
+    "revision": "eccaa9d0722620ec901f",
+    "url": "/Rich-Text-Editor/static/css/main.3fe58bd1.chunk.css"
+  },
+  {
+    "revision": "ce51a405ddcba0ff0abf",
+    "url": "/Rich-Text-Editor/static/js/2.39a48eba.chunk.js"
+  },
+  {
+    "revision": "eccaa9d0722620ec901f",
+    "url": "/Rich-Text-Editor/static/js/main.4c7d0b5b.chunk.js"
+  },
+  {
+    "revision": "28b2074a41a5ca1cbd03",
+    "url": "/Rich-Text-Editor/static/js/runtime-main.641fef0c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Rich-Text-Editor/static/media/logo.25bf045c.svg"
+  }
+]);
